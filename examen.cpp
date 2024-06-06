5654c90a45205ac6ac7fc0c6d7d5cc01f5896dd6
Robert5Alen <160903420+Robert5Alen@users.noreply.github.com>
2024-06-06 08:47:18 -0400

    P7

--- examen.cpp
+++ examen.cpp
@@ -1,25 +1,41 @@
 #include<iostream>
 #include<map>
 #include<vector>
+#include <string>
 
 using namespace std;
 
 struct comp_events{
-
-
+ 
 
 
 };
 
- 
+void find(vector<string> a);
+
+void print(vector<string> d);
+
+vector <string> add(vector<string> b);
+
+vector <string> del(vector<string> c);
+
+
+
+
+
+
 int main()
 {
-//Entradadas
-
-
-
+//entradas
+string operacion;
 
 map<string,string> dinam;
+
+int cont;
+
+vector<vector<string>> data;
+
+vector<int> dias;
 
 
 
