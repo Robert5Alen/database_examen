b93e1f16757134ea5a7f6e7fc753fdef26abe6f2
Robert5Alen <160903420+Robert5Alen@users.noreply.github.com>
2024-06-15 18:45:29 -0400

    P10

--- examen.cpp
+++ examen.cpp
@@ -11,7 +11,7 @@ struct comp_events{
 
 };
 
-void find(vector<string> a);
+
 
 void print(vector<string> d);
 
@@ -24,14 +24,58 @@ vector <string> del(vector<string> c);
 int main()
 {
 
-cout<<"Presione el comando a realizar: "<<endl;
+vector<string>datos;
+string comando;
+string fecha;
+string evento;
+string cadena;
+char n;
+
+    
+ map<string, string> datos;   
+ map<string,string>::iterator i;
 
 string operacion;
 
+cout<<"Presione la operacion a realizar o n para salir : "<<endl<<endl;
+
+cout<<"El formato debe ser el siguienteseparados por espacion: Comando Fecha(Año-Mes-Día) Evento"<<endl;
+
 while(cin>>operacion)
 {
+int cont=0;
 
-if(operacion!="Add"||operacion!="Del"||operacion!="Find"||operacion!="Print")
+for(int a=0;operacion.size()>a;a++)
+{
+
+int spc;
+if(operacion[a]==' ')
+{
+spc=a;
+
+for(cont;spc>cont;cont++)
+{
+
+cadena+=operacion[cont];
+
+}
+
+
+}
+
+
+
+}
+
+
+
+
+
+
+}
+
+
+/*if(operacion!="Add"||operacion!="Del"||operacion!="Find"||operacion!="Print"||operacion!="n")
 {
 
 cout<<"Unknown command: "<<operacion<<endl;
@@ -40,34 +84,28 @@ break;
 }
 
 
-if(dinam)
+if(operacion=="n")
 {
 
+cout<<"Salida de ejecucion exitosa"<<endl;
 
-
-
-}
-
-
+break;
 
 }
 
+while( true)
+{
+ cout<<"Introduce la fecha en formato Año-Mes-Día seguido el evento y para salir n"<<endl;
+ 
+ cin>>fecha;
+ cin>>evento;
+ 
+ datos[fecha]=evento;
 
 
 
 
-
-
-map<string,string> dinam;
-
-int cont;
-
-vector<vector<string>> data;
-
-vector<int> dias;
-
-
-
+}*/
 
 
     return 0;
